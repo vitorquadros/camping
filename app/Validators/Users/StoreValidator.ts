516967bcc6f1512d21358792e3fc96d11c64cf64
Vitor Quadros <viitorquadros@hotmail.com>
2021-10-18 17:32:45 -0300

    feat: create StoreValidator for UserController

---
+++ app/Validators/Users/StoreValidator.ts
@@ -0,0 +1,26 @@
+import { schema, rules } from '@ioc:Adonis/Core/Validator';
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
+
+export default class StoreValidator {
+  constructor(protected ctx: HttpContextContract) {}
+
+  public schema = schema.create({
+    fullName: schema.string({ trim: true }),
+    username: schema.string({ trim: true }),
+    email: schema.string({ trim: true }, [
+      rules.email(),
+      rules.unique({ table: 'users', column: 'email' })
+    ]),
+    password: schema.string({ trim: true }, [rules.confirmed('passwordConfirmation')]),
+    cpf: schema.string({ trim: true }, [rules.minLength(11), rules.maxLength(11)]),
+    birthday: schema.string({ trim: true }), // TODO: validate date
+
+    state: schema.string({ trim: true }),
+    city: schema.string({ trim: true }),
+    street: schema.string({ trim: true }),
+    streetNumber: schema.number(),
+    zipCode: schema.string({ trim: true })
+  });
+
+  public messages = {};
+}

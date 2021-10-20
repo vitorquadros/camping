db9bdd895e20a71a634ea93fe2ee32de244f8e76
Vitor Quadros <viitorquadros@hotmail.com>
2021-10-20 17:24:16 -0300

    feat: preload address on User model

--- app/Models/User.ts
+++ app/Models/User.ts
@@ -1,6 +1,15 @@
 import { DateTime } from 'luxon';
 import Hash from '@ioc:Adonis/Core/Hash';
-import { column, beforeSave, BaseModel, belongsTo, BelongsTo } from '@ioc:Adonis/Lucid/Orm';
+import {
+  column,
+  beforeSave,
+  BaseModel,
+  belongsTo,
+  BelongsTo,
+  beforeFind,
+  ModelQueryBuilderContract,
+  beforeFetch
+} from '@ioc:Adonis/Lucid/Orm';
 import { Address } from 'App/Models';
 
 export default class User extends BaseModel {
@@ -57,6 +66,12 @@ export default class User extends BaseModel {
     if (user.$dirty.password) {
       user.password = await Hash.make(user.password);
     }
+  }
+
+  @beforeFetch()
+  @beforeFind()
+  public static fetchAddress(query: ModelQueryBuilderContract<typeof User>) {
+    query.preload('address');
   }
 
   @belongsTo(() => Address)

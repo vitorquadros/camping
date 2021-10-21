6436c0a28b07cfa4499ac6704091d723b5101532
Vitor Quadros <viitorquadros@hotmail.com>
2021-10-21 16:48:23 -0300

    feat: create Tour model

---
+++ app/Models/Tour.ts
@@ -0,0 +1,47 @@
+import { DateTime } from 'luxon';
+import { BaseModel, column, HasOne, hasOne } from '@ioc:Adonis/Lucid/Orm';
+import { Address } from 'App/Models';
+
+export default class Tour extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number;
+
+  @column()
+  public name: string;
+
+  @column()
+  public slug: string;
+
+  @column()
+  public description: string;
+
+  @column()
+  public private: boolean;
+
+  @column()
+  public difficulty: 'easy' | 'experient' | 'expert';
+
+  @column()
+  public duration: number;
+
+  @column()
+  public maxGroupSize: number;
+
+  @column()
+  public price: number;
+
+  @column()
+  public startLocationId: number;
+
+  @column.dateTime()
+  public startDate: DateTime;
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime;
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime;
+
+  @hasOne(() => Address)
+  public address: HasOne<typeof Address>;
+}
--- app/Models/index.ts
+++ app/Models/index.ts
@@ -1,2 +1,3 @@
 export { default as User } from './User';
 export { default as Address } from './Address';
+export { default as Tour } from './Tour';

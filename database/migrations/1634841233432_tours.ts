8051f6a1cec7b91fc7868ec93db33e0ee169c7df
Vitor Quadros <viitorquadros@hotmail.com>
2021-10-21 15:47:01 -0300

    feat: create tours migration

---
+++ database/migrations/1634841233432_tours.ts
@@ -0,0 +1,41 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema';
+
+export default class Tours extends BaseSchema {
+  protected tableName = 'tours';
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').primary();
+      table.string('name').notNullable();
+      table.string('slug').notNullable();
+      table.text('description').notNullable();
+      table.boolean('private').defaultTo(false);
+      table
+        .enum('difficulty', ['easy', 'experient', 'expert'], {
+          useNative: true,
+          enumName: 'tour_difficulties'
+        })
+        .notNullable();
+      table.integer('duration').notNullable();
+      table.integer('max_group_size').notNullable();
+      table.decimal('price', 10, 2).notNullable();
+      table
+        .integer('start_location_id')
+        .notNullable()
+        .unsigned()
+        .references('id')
+        .inTable('tours_locations')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+      table.datetime('start_date').notNullable();
+
+      table.timestamp('created_at', { useTz: true });
+      table.timestamp('updated_at', { useTz: true });
+    });
+  }
+
+  public async down() {
+    this.schema.raw('DROP TYPE IF EXISTS "tour_difficulties"');
+    this.schema.dropTable(this.tableName);
+  }
+}
